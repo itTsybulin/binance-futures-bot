0295eeba685a1983cd31d3bc4c4b292c4143c863
Alexandr <ittsybulin@yandex.ru>
2023-02-21 21:35:44 +0300

    Update main.py

--- python_files/main.py
+++ python_files/main.py
@@ -1,41 +1,40 @@
 from binance.um_futures import UMFutures
 
-# ключи API
-api_key = "<your_api_key>"
-api_secret = "<your_secret_key>"
+# ключи API и прочее
+API_KEY = "<your_api_key>"
+API_SECRET = "<your_secret_key>"
 
-# переменные
-symbol = "BTCUSDT"
-timeframe = "1h"
+SYMBOL = "BTCUSDT"
+TIMEFRAME = "1h"
+
 ema_fast = 8
 ema_slow = 21
 
-# создание клиента и получение данных
-client = UMFutures(api_key, api_secret)
+def get_data(client):
+	""" получение данных """
+	return	client.klines(SYMBOL, TIMEFRAME)
 
-# получение данных
-def get_data():
-	data = client.klines(symbol, timeframe)
-	return data
-
-# получение цен закрытия последних 30 свечей
 def get_price_list(data):
+	""" получение цен закрытия последних 30 свечей """
 	list = []
-	del data[0:len(data) - ema_slow]
-	for i in range(ema_slow):
-		list.append(data[i][4]) # 4 элемент, это цена закрытия в коллекции
+	for element in data[len(data) - ema_slow:len(data)]:
+		list.append(element[4]) # цена закрытия
 	return list
 
-# получение последней цены
 def get_current_price():
-	current_price = get_price_list(get_data())
+	""" получение последней цены """
+	current_price = get_price_list(get_data(client))
 	return current_price[-1]
 
 # расчет EMA
 def EMA_calculate(data, period):
+	"""  """
 	pass
 
 def main():
+	# создание клиента
+	client = UMFutures(API_KEY, API_SECRET)
+	
 	data = get_data()
 	print(get_current_price())
 	print(get_price_list(data))
